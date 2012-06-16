58d525bd23c30217e566dcc27755e060abe7d7f2
Italo Maia <italo.maia@gmail.com>
2012-06-16 01:57:06 -0300

    jars adicionados e testes =D

---
+++ src/br/com/custoviagem/tests/TestDecolar.java
@@ -0,0 +1,14 @@
+package br.com.custoviagem.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDecolar {
+
+	@Test
+	public void testSearch() {
+		fail("Not yet implemented");
+	}
+
+}
